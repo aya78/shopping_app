fcb5bedfe4e43a6335c8c29c648e5f1f36b8c5f7
aya123456+++ <ayak77431@gmail.com>
2023-02-15 16:24:24 +0200

    new

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,2 +1,59 @@
-package pages;public class HomePage {
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class HomePage {
+    private static WebElement element=null;
+
+    // public int randomInt ;
+    static WebDriver driver = null;
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+    }
+    public static WebElement click_on_myAccount(WebDriver driver){
+        element = driver.findElement(By.xpath("//*[@id=\"top-links\"]/ul/li[2]/a"));
+        return element;
+    }
+    // //*[@id="top-links"]/ul/li[2]/ul/li[2]/a
+    public static WebElement navigate_to_login(WebDriver driver){
+        element = driver.findElement(By.xpath("//*[@id=\"top-links\"]/ul/li[2]/ul/li[2]/a"));
+        return element;
+    }
+    // //*[@id="input-email"]
+    public static WebElement enter_email(WebDriver driver){
+        element = driver.findElement(By.xpath("//*[@id=\"input-email\"]"));
+        return element;
+    }
+    // //*[@id="input-password"]
+    public static WebElement enter_password(WebDriver driver){
+        element = driver.findElement(By.xpath("//*[@id=\"input-password\"]"));
+        return element;
+    }
+    // //*[@id="content"]/div/div[2]/div/form/input
+    public static WebElement click_login(WebDriver driver){
+        element = driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[2]/div/form/input"));
+        return element;
+    }
+    // //*[@id="content"]/div[2]/div[1]/div/div[3]/button[2]
+    public static WebElement add_favorite1(WebDriver driver){
+        element = driver.findElement(By.xpath("//*[@id=\"content\"]/div[2]/div[1]/div/div[3]/button[2]"));
+        return element;
+    }
+    // //*[@id="content"]/div[2]/div[3]/div/div[3]/button[2]
+    public static WebElement add_favorite2(WebDriver driver){
+        element = driver.findElement(By.xpath("//*[@id=\"content\"]/div[2]/div[3]/div/div[3]/button[2]"));
+        return element;
+    }
+    // //*[@id="content"]/div[2]/div[2]/div/div[3]/button[3]
+    public static WebElement click_compare(WebDriver driver){
+        element = driver.findElement(By.xpath("//*[@id=\"content\"]/div[2]/div[2]/div/div[3]/button[3]"));
+        return element;
+    }
+    // //*[@id="content"]/div[2]/div[1]/div/div[1]/a/img
+    public static WebElement open_product(WebDriver driver){
+        element = driver.findElement(By.xpath("//*[@id=\"content\"]/div[2]/div[1]/div/div[1]/a/img"));
+        return element;
+    }
 }
--- src/test/java/tests/scrollingInHome.java
+++ src/test/java/tests/scrollingInHome.java
@@ -1,2 +1,53 @@
-package tests;public class scrollingInHome {
+package tests;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import pages.HomePage;
+
+public class scrollingInHome {
+    /*
+    *
+        //scroll up a page
+        a.sendKeys(Keys.PAGE_DOWN).build().perform();*/
+    String random_number = RandomStringUtils.random(5, false, true);
+    String random_barcode = RandomStringUtils.random(8, false, true);
+    public String random_string = RandomStringUtils.random(6, true, false);
+
+    public WebDriver driver;
+    @BeforeTest(description = "SetUp chrome driver")
+    public void SetUp()
+    {
+        System.setProperty("webdriver.chrome.driver","/home/hash-pc-8/IdeaProjects/Gadawl/src/test/resources/chromedriver");
+        driver=new ChromeDriver();
+    }
+    @Test(priority = 0)
+    public void Valid_login() throws InterruptedException {
+        driver.manage().window().maximize();
+        driver.get("http://tutorialsninja.com/demo/index.php?route=common/home");
+        HomePage.click_on_myAccount(driver).click();
+        HomePage.navigate_to_login(driver).click();
+        HomePage.enter_email(driver).sendKeys("aya7777@gmail.com");
+        HomePage.enter_password(driver).sendKeys("" + 123456789);
+        HomePage.click_login(driver).click();
+        Thread.sleep(2000);
+    }
+    @Test(priority = 1)
+    public void open_productsToChart() throws InterruptedException {
+
+        driver.findElement(By.xpath("//*[@id=\"account-account\"]/ul/li[1]/a")).click();
+        Actions a = new Actions(driver);
+        a.sendKeys(Keys.PAGE_DOWN).build().perform();
+         HomePage.add_favorite1(driver).click();
+         HomePage.add_favorite2(driver).click();
+         HomePage.click_compare(driver).click();
+         HomePage.open_product(driver).click();
+
+        Thread.sleep(2000);
+    }
 }
